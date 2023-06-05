925d0d015df1aef499af9f62efecd5e4e640a4f1
mike-ninja <mikeedubas@hotmaiL.com>
2023-06-05 19:57:07 +0300

    completed ex 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [view, setView] = useState(false)
+
+  const hideWhenVisible = { display: view ? 'none' : '' }
+  const showWhenVisible = { display: view ? '' : 'none' }
+
+  const toggleView = () => {
+    setView(!view)
+  }
+
+  return (
+    <div className='blog'>
+      {blog.title}
+      <button style={hideWhenVisible} onClick={toggleView}>
+        view
+      </button>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <div>
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <p>{blog.author}</p>
+        <button onClick={toggleView}>
+          hide
+        </button>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
